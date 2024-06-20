0410c63bdeff6d1f28a90fd627419ea7994304e7
CoolDoge <thedarkguard60@gmail.com>
2024-06-20 19:33:22 +0500

    Implement AppContainer

---
+++ app/src/main/java/com/dogiumlabs/coldweather/ColdWeatherApplication.kt
@@ -0,0 +1,15 @@
+package com.dogiumlabs.coldweather
+
+import android.app.Application
+import com.dogiumlabs.coldweather.data.AppContainer
+import com.dogiumlabs.coldweather.data.WeatherApiAppContainer
+
+class ColdWeatherApplication: Application() {
+    /** AppContainer instance used by the rest of classes to obtain dependencies */
+    lateinit var container: AppContainer
+
+    override fun onCreate() {
+        super.onCreate()
+        container = WeatherApiAppContainer()
+    }
+}
--- app/src/main/java/com/dogiumlabs/coldweather/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/dogiumlabs/coldweather/ui/home/HomeViewModel.kt
@@ -1,4 +1,7 @@
 package com.dogiumlabs.coldweather.ui.home
 
-class HomeViewModel {
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel: ViewModel() {
+
 }
