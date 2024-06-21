ec127c45c743d9b77ef3c923d4e2affbe5502945
CoolDoge <thedarkguard60@gmail.com>
2024-06-21 20:36:11 +0500

    Upgrade AGP, replace @SerialName with GSON's @SerializedName

--- app/src/main/java/com/dogiumlabs/coldweather/data/Weather.kt
+++ app/src/main/java/com/dogiumlabs/coldweather/data/Weather.kt
@@ -1,63 +1,64 @@
 package com.dogiumlabs.coldweather.data
 
+import com.google.gson.annotations.SerializedName
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class Weather(
-    @SerialName("location") val location: Location,
-    @SerialName("current") val current: Current
+    @SerializedName("location") val location: Location,
+    @SerializedName("current") val current: Current
 )
 
 @Serializable
 data class Location(
-    @SerialName("name") val name: String,
-    @SerialName("region") val region: String,
-    @SerialName("country") val country: String,
-    @SerialName("lat") val latitude: Double,
-    @SerialName("lon") val longitude: Double,
-    @SerialName("tz_id") val timeZoneId: String,
-    @SerialName("localtime_epoch") val localTimeEpoch: Long,
-    @SerialName("localtime") val localTime: String
+    @SerializedName("name") val name: String,
+    @SerializedName("region") val region: String,
+    @SerializedName("country") val country: String,
+    @SerializedName("lat") val latitude: Double,
+    @SerializedName("lon") val longitude: Double,
+    @SerializedName("tz_id") val timeZoneId: String,
+    @SerializedName("localtime_epoch") val localTimeEpoch: Long,
+    @SerializedName("localtime") val localTime: String
 )
 
 @Serializable
 data class Current(
-    @SerialName("last_updated_epoch") val lastUpdatedEpoch: Long,
-    @SerialName("last_updated") val lastUpdated: String,
-    @SerialName("temp_c") val tempC: Double,
-    @SerialName("temp_f") val tempF: Double,
-    @SerialName("is_day") val isDay: Boolean,
-    @SerialName("condition") val condition: Condition,
+    @SerializedName("last_updated_epoch") val lastUpdatedEpoch: Long,
+    @SerializedName("last_updated") val lastUpdated: String,
+    @SerializedName("temp_c") val tempC: Double,
+    @SerializedName("temp_f") val tempF: Double,
+    @SerializedName("is_day") val isDay: Byte,
+    @SerializedName("condition") val condition: Condition,
 
-    @SerialName("wind_mph") val windMph: Double,
-    @SerialName("wind_kph") val windKph: Double,
-    @SerialName("wind_degree") val windDegree: Int,
-    @SerialName("wind_dir") val windDirection: String,
-    @SerialName("pressure_mb") val pressureMb: Int,
-    @SerialName("pressure_in") val pressureIn: Double,
-    @SerialName("precip_mm") val precipMm: Double,
-    @SerialName("precip_in") val precipIn: Double,
+    @SerializedName("wind_mph") val windMph: Double,
+    @SerializedName("wind_kph") val windKph: Double,
+    @SerializedName("wind_degree") val windDegree: Int,
+    @SerializedName("wind_dir") val windDirection: String,
+    @SerializedName("pressure_mb") val pressureMb: Int,
+    @SerializedName("pressure_in") val pressureIn: Double,
+    @SerializedName("precip_mm") val precipMm: Double,
+    @SerializedName("precip_in") val precipIn: Double,
     val humidity: Int,
     val cloud: Int,
-    @SerialName("feelslike_c") val feelsLikeCelsius: Double,
-    @SerialName("feelslike_f") val feelsLikeFahrenheit: Double,
-    @SerialName("windchill_c") val windChillCelsius: Double,
-    @SerialName("windchill_f") val windChillFahrenheit: Double,
-    @SerialName("heatindex_c") val heatIndexCelsius: Double,
-    @SerialName("heatindex_f") val heatIndexFahrenheit: Double,
-    @SerialName("dewpoint_c") val dewPointCelsius: Double,
-    @SerialName("dewpoint_f") val dewPointFahrenheit: Double,
-    @SerialName("vis_km") val visibilityKm: Int,
-    @SerialName("vis_miles") val visibilityMiles: Int,
+    @SerializedName("feelslike_c") val feelsLikeCelsius: Double,
+    @SerializedName("feelslike_f") val feelsLikeFahrenheit: Double,
+    @SerializedName("windchill_c") val windChillCelsius: Double,
+    @SerializedName("windchill_f") val windChillFahrenheit: Double,
+    @SerializedName("heatindex_c") val heatIndexCelsius: Double,
+    @SerializedName("heatindex_f") val heatIndexFahrenheit: Double,
+    @SerializedName("dewpoint_c") val dewPointCelsius: Double,
+    @SerializedName("dewpoint_f") val dewPointFahrenheit: Double,
+    @SerializedName("vis_km") val visibilityKm: Int,
+    @SerializedName("vis_miles") val visibilityMiles: Int,
     val uv: Int,
-    @SerialName("gust_mph") val gustMph: Double,
-    @SerialName("gust_kph") val gustKph: Double
+    @SerializedName("gust_mph") val gustMph: Double,
+    @SerializedName("gust_kph") val gustKph: Double
 )
 
 @Serializable
 data class Condition(
-    @SerialName("text") val text: String,
-    @SerialName("icon") val icon: String,
-    @SerialName("code") val code: Int
+    @SerializedName("text") val text: String,
+    @SerializedName("icon") val icon: String,
+    @SerializedName("code") val code: Int
 )
--- app/src/main/java/com/dogiumlabs/coldweather/ui/home/HomeScreen.kt
+++ app/src/main/java/com/dogiumlabs/coldweather/ui/home/HomeScreen.kt
@@ -42,7 +42,7 @@ fun HomeScreen(
     when (homeUiState) {
         HomeUiState.Error -> Text(text = "error")
         HomeUiState.Loading -> Text(text = "loading")
-        is HomeUiState.Success -> Text(text = "Data is ${homeUiState.weather.current.uv}")
+        is HomeUiState.Success -> Text(text = "Data is ${homeUiState.weather.current.tempC}")
     }
 
     /** Screen that displays basic weather info **/
