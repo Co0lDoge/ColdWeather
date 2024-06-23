7a41ee2ada21c32622da1981d6229631bbadef8d
CoolDoge <thedarkguard60@gmail.com>
2024-06-23 23:29:51 +0500

    Change status bar's color to background color, currently disable dynamic color.

--- app/src/main/java/com/dogiumlabs/coldweather/ui/theme/Theme.kt
+++ app/src/main/java/com/dogiumlabs/coldweather/ui/theme/Theme.kt
@@ -1,6 +1,7 @@
 package com.dogiumlabs.coldweather.ui.theme
 
 import android.app.Activity
+import android.graphics.Color.toArgb
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.material3.MaterialTheme
@@ -41,7 +42,7 @@ private val LightColorScheme = lightColorScheme(
 fun ColdWeatherTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
-    dynamicColor: Boolean = true,
+    dynamicColor: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
@@ -57,8 +58,8 @@ fun ColdWeatherTheme(
     if (!view.isInEditMode) {
         SideEffect {
             val window = (view.context as Activity).window
-            window.statusBarColor = colorScheme.primary.toArgb()
-            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = darkTheme
+            window.statusBarColor = colorScheme.background.toArgb()
+            WindowCompat.getInsetsController(window, view).isAppearanceLightStatusBars = !darkTheme
         }
     }
 
